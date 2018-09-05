4c2765f978b0d03c2c1a30c867b39b8c74807c7a
Sergey Shramko <42001531+Shramkoweb@users.noreply.github.com>
2018-09-05 13:11:34 +0300

    Карта

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {
-      lat: 34.871002,
-      lng: -111.760826
+      lat: 34.8543583,
+      lng: -111.8651786
     },
-    zoom: 8
+    zoom: 10
   });
 }

c2135258aadf0fb3982c1e64874c40f4ef99a88b
Sergey Shramko <42001531+Shramkoweb@users.noreply.github.com>
2018-09-05 16:49:21 +0300

    Добавляет анимацию и проверку заполнения форм

--- js/popup.js
+++ js/popup.js
@@ -1,7 +1,19 @@
 var link = document.querySelector(".btn-appointment");
 var popup = document.querySelector(".modal-form");
+var arrival = popup.querySelector("[name=arrival-date]");
+var departure = popup.querySelector("[name=departure-date]");
+var adult = popup.querySelector("[name=adult]");
+var kids = popup.querySelector("[name=kids]");
+var form = popup.querySelector("form");
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault();
   popup.classList.toggle("modal-show");
+  arrival.focus();
+});
+
+form.addEventListener("submit", function (evt) {
+  if (!arrival.value || !departure.value || !adult.value || !kids.value) {
+    evt.preventDefault();
+    alert("Нужно ввести все значения");
+  }
 });

3919ebf280af1fa2a7c42abfaedafef4fced1647
Sergey Shramko <42001531+Shramkoweb@users.noreply.github.com>
2018-09-07 15:29:49 +0300

    Убирает стилизацию по id

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var map;
 
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map'), {
+  map = new google.maps.Map(document.querySelector('.map-wrapper'), {
     center: {
       lat: 34.8543583,
       lng: -111.8651786
